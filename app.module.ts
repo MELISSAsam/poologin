63f02bf272f28a72060b65819a57376f19e91bb7
MELISSAsam <smchuqui@sudamericano.edu.ec>
2025-05-20 08:51:43 -0500

    Add files via upload

---
+++ app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres', // tu usuario de postgres
+      password: '0150102051.s.ch',    // tu contraseña
+      database: 'login_app_db',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    UsersModule,
+    AuthModule,
+  ],
+})
+export class AppModule {}
